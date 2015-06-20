e33999a6bc1dc314bec182c8f6592a7734121d1a
Niu Guangyuan <u0705666@gmail.com>
2015-06-20 18:28:56 +0800

    implementing UI

--- app/src/main/java/com/ngy/mymud/ui/widget/CharacterStatusBar.java
+++ app/src/main/java/com/ngy/mymud/ui/widget/CharacterStatusBar.java
@@ -1,7 +1,25 @@
 package com.ngy.mymud.ui.widget;
 
+import android.content.Context;
+import android.widget.LinearLayout;
+
+import com.ngy.mymud.R;
+
+import java.util.zip.Inflater;
+
 /**
  * Created by niuguangyuan on 6/20/2015.
  */
-public class CharacterStatusBar {
+public class CharacterStatusBar extends LinearLayout {
+
+    public CharacterStatusBar(Context context) {
+        super(context);
+        init(context);
+    }
+
+    private void init(Context context) {
+        inflate(context, R.layout.character_status_bar_view_layout, this);
+    }
+
+
 }
