35c11e6d994dc663e8ae2d6f25c4bdfb32a61c64
Niu Guangyuan <u0705666@gmail.com>
2015-06-23 23:54:17 +0800

    implement and test bookview

--- app/src/main/java/com/ngy/mymud/ui/BattleView.java
+++ app/src/main/java/com/ngy/mymud/ui/BattleView.java
@@ -1,11 +1,14 @@
 package com.ngy.mymud.ui;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
 import com.ngy.mymud.R;
+import com.ngy.mymud.ui.widget.BookView;
 import com.ngy.mymud.ui.widget.CharacterStatusBar;
 
 import org.androidannotations.annotations.AfterViews;
@@ -25,12 +28,24 @@ public class BattleView extends RelativeLayout {
     @ViewById(R.id.testShowHealthChange)
     protected Button mTestShow;
 
+    @ViewById(R.id.battle_description)
+    protected BookView mBookView;
+
     public BattleView(Context context) {
         super(context);
     }
 
     @AfterViews
     protected void init() {
+        final Handler handler = new Handler(Looper.getMainLooper());
+        for (int i = 0; i < 40; i++) {
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mBookView.addLine("hello, world");
+                }
+            }, 1000 * i);
+        }
         mPlayerStatus.setUsername("牛广远");
         mPlayerStatus.setMaxValue(100, 100);
         mPlayerStatus.setCurrentValue(50, 80);
--- app/src/main/java/com/ngy/mymud/ui/widget/BookView.java
+++ app/src/main/java/com/ngy/mymud/ui/widget/BookView.java
@@ -1,11 +1,17 @@
 package com.ngy.mymud.ui.widget;
 
 import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.ngy.mymud.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by niuguangyuan on 6/21/15.
@@ -13,6 +19,7 @@ import com.ngy.mymud.R;
 public class BookView extends RelativeLayout {
 
     protected RecyclerView mBodyListView;
+    protected BookAdapter mAdapter;
 
     public BookView(Context context) {
         super(context);
@@ -27,6 +34,10 @@ public class BookView extends RelativeLayout {
     private void init(Context context) {
         inflate(context, R.layout.book_view_layout, this);
         mBodyListView = (RecyclerView) findViewById(R.id.body_listview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(context);
+        mBodyListView.setLayoutManager(layoutManager);
+        mAdapter = new BookAdapter(context);
+        mBodyListView.setAdapter(mAdapter);
     }
 
     /**
@@ -34,12 +45,66 @@ public class BookView extends RelativeLayout {
      * @param line
      */
     public void addLine(String line) {
-
+        if (mAdapter != null) {
+            mAdapter.addLine(line);
+            mBodyListView.getLayoutManager().scrollToPosition(mAdapter.mMsgList.size()-1);
+        }
     }
 
     /**
      * clear the content of the book
      */
-    public void clear() {}
+    public void clear() {
+        if (mAdapter != null) {
+            mAdapter.clear();
+        }
+    }
 
+    public static class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder> {
+
+        private List<CharSequence> mMsgList;
+        private Context mContext;
+
+        public BookAdapter(Context context) {
+            mContext = context;
+            mMsgList = new ArrayList<>();
+        }
+
+        public void addLine(CharSequence line) {
+            mMsgList.add(line);
+            notifyDataSetChanged();
+        }
+
+        public void clear() {
+            mMsgList.clear();
+            notifyDataSetChanged();
+        }
+
+        public static class ViewHolder extends RecyclerView.ViewHolder  {
+
+            TextView mTextView;
+
+            public ViewHolder(TextView itemView) {
+                super(itemView);
+                mTextView = itemView;
+            }
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            TextView textView = new TextView(mContext);
+            ViewHolder viewHolder = new ViewHolder(textView);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.mTextView.setText(mMsgList.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mMsgList.size();
+        }
+    }
 }
